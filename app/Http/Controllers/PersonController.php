0a329bb75e19c7d1ca0fbf71f96c9fbac5c3395e
pharreX19 <pharreX19@gmail.com>
2025-01-28 22:55:56 +0500

    feat: added person search endpoint

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+
+    /**
+     * Search the specified resource from storage.
+     */
+    public function search(String $nid)
+    {
+        return Person::where('national_id', $nid)->first();
+    }
+}
