600bbf6f8e26a47656ba3053335f4a75808b7361
pharreX19 <pharreX19@gmail.com>
2025-02-02 22:51:56 +0500

    code refactor

--- resources/js/Components/AutoComplete.vue
+++ resources/js/Components/AutoComplete.vue
@@ -1,17 +1,10 @@
 <template>
     <div class="w-full relative">
-        <!-- <input
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            type="text"
-            name="title"
-            id="title"
-            :value="modelValue?.name"
-            @input="handleInput"
-        /> -->
         <ThaanaInput
             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             :value="modelValue.name"
             @input="handleInput"
+            :placeholder="placeholder"
         />
         <ul
             class="mt-1 w-full max-h-60 border border-gray-200 rounded-md bg-white absolute overflow-y-auto z-20"
@@ -37,6 +30,10 @@ const props = defineProps({
     modelValue: {
         type: Object,
         required: true,
+    },
+    placeholder: {
+        type: String,
+        required: false,
     },
 
     // fieldValue: {
