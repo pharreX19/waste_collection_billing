7758da85701c672dc8ba86f7c03da009f904b0c8
pharreX19 <pharreX19@gmail.com>
2025-02-03 11:27:34 +0500

    feat: states for payable and payment

---
+++ app/Constants/Payable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Constants;
+
+use App\Constants\Constant;
+
+class Payable extends Constant
+{
+    const PENDING = 'pending';
+    const PAID = 'paid';
+    const OVERDUE = 'overdue';
+    const CANCELLED = 'cancelled';
+
+
+    const PENDING_STATES = [
+        'pending',
+        'overdue'
+    ];
+}
---
+++ app/Constants/Payment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+use App\Constants\Constant;
+
+class Payment extends Constant
+{
+    const CONFIRMED = 'confirmed';
+    const CANCELLED = 'cancelled';
+    const PENDING = 'pending';
+}
