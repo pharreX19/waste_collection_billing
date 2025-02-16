505845cafa99610b94f1c7f72bad3691a0ca8a1b
pharreX19 <pharreX19@gmail.com>
2025-02-17 00:16:01 +0500

    fix: code refactor

--- resources/js/Pages/Settings/Index.vue
+++ resources/js/Pages/Settings/Index.vue
@@ -23,7 +23,7 @@
             </div>
             <div class="hidden sm:block">
                 <div class="border-b border-gray-200">
-                    <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+                    <nav class="-mb-px flex" aria-label="Tabs">
                         <Link
                             v-for="tab in tabs"
                             :key="tab.name"
@@ -33,7 +33,7 @@
                                 tab.current
                                     ? 'border-indigo-500 text-indigo-600'
                                     : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                                'whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium',
+                                'whitespace-nowrap border-b-2 px-8 py-4 text-sm font-medium',
                             ]"
                             :aria-current="tab.current ? 'page' : undefined"
                             >{{ tab.name }}</Link
--- resources/js/Pages/Settings/Payables.vue
+++ resources/js/Pages/Settings/Payables.vue
@@ -1,7 +1,113 @@
 <template>
-    <div>Payables</div>
+    <div class="px-4 sm:px-6 lg:px-8 max-w-2xl">
+        <div>
+            <dl class="mt-6 divide-y divide-gray-100 text-sm/6">
+                <div
+                    class="py-6 sm:flex"
+                    v-for="(item, index) in data"
+                    :key="item.id"
+                >
+                    <dt
+                        class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6"
+                    >
+                        {{ keyMapping(item.key) }}
+                    </dt>
+                    <dd
+                        class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto"
+                    >
+                        <div class="text-gray-900">
+                            {{
+                                index == 0
+                                    ? `${NumberFormatter.format(
+                                          item.value
+                                      )} ރުފިޔާ`
+                                    : `${item.value} ދުވަސް`
+                            }}
+
+                            <transition name="slide">
+                                <input
+                                    v-if="activeIndex === index"
+                                    type="number"
+                                    step="0.1"
+                                    name="fine_amount"
+                                    id="fine_amount"
+                                    dir="ltr"
+                                    v-model="form[index].value"
+                                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                />
+                            </transition>
+                        </div>
+
+                        <button
+                            type="button"
+                            @click="toggleEdit(index)"
+                            class="font-semibold text-indigo-600 hover:text-indigo-500"
+                        >
+                            {{
+                                activeIndex === index
+                                    ? "ރައްކާ ކުރުން"
+                                    : "އެޑިޓް ކުރުން"
+                            }}
+                        </button>
+                    </dd>
+                </div>
+            </dl>
+        </div>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { NumberFormatter } from "../../utils/numberFormatter";
+import { useForm } from "@inertiajs/vue3";
+const props = defineProps({
+    data: {
+        type: Array,
+        required: true,
+    },
+});
 
-<style lang="scss" scoped></style>
+const form = useForm(props.data);
+
+const mappings = {
+    fine_amount: "ޖޫރިމަނާ ކުރެވޭ ޢަދަދު",
+    validity_period: "ބިލް ދައްކަން ލިބޭ މުއްދަތު",
+};
+
+const activeIndex = ref(null);
+
+const toggleEdit = (index) => {
+    activeIndex.value = activeIndex.value === index ? null : index;
+    if (activeIndex.value === null) {
+        updateSetting();
+    }
+};
+
+const updateSetting = async () => {
+    form.put(route("settings.update"), {
+        preserveScroll: true,
+    });
+};
+
+const keyMapping = (key) => {
+    return mappings[key];
+};
+</script>
+
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+    transition: max-height 0.3s ease-out, opacity 0.3s ease;
+    overflow: hidden;
+}
+.slide-enter-from,
+.slide-leave-to {
+    max-height: 0;
+    opacity: 0;
+}
+.slide-enter-to,
+.slide-leave-from {
+    max-height: 100px; /* Adjust as needed */
+    opacity: 1;
+}
+</style>
