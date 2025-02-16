97ecc824298bacacaccdb793b739d339d4e82496
pharreX19 <pharreX19@gmail.com>
2025-02-16 21:25:29 +0500

    feat: added user request

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email,' . $this->route('user')?->id,
+            'password' => 'required_if:method,POST|nullable|string|min:5|max:20|confirmed',
+            'password_confirmation' => 'required_with:password',
+            'is_active' => 'sometimes|nullable|boolean'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'ފުރިހަމަ ނަން ލިޔުއްވާ',
+            'name.string' => 'ފުރިހަމަ ނަން ލިޔުއްވާ',
+
+            'email.required' => 'އީމެއިލް ލިޔުއްވާ',
+            'email.email' => 'އީމެއިލް ރަނގަޅެއް ނޫން',
+            'email.unique' => 'އީމެއިލް ވަނީ ކުރިން ރަޖިސްޓަރ ކޮށްފައި',
+
+            'password_confirmation.required' => 'ޕާސްވާރޑް ލިޔުއްވާ',
+
+            'password.required' => 'ޕާސްވާރޑް ލިޔުއްވާ',
+            'password.confirmed' => 'ޕާސްވާރޑް އަދި ޕާސްވާރޑް ކޮންފަރމޭޝަން ދިމައެއް ނުވޭ',
+            'password.string' => 'ޕާސްވާރޑް ނޫން',
+            'password.min' => 'ޕާސްވާރޑްގެ ދިގުމިން އަދި ރަނގަޅެއްނޫން',
+            'password.max' => 'ޕާސްވާރޑްގެ ދިގުމިން އަދި ރަނގަޅެއްނޫން'
+        ];
+    }
+}
