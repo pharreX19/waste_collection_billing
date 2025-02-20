b507cf0391527ec42d408cc935f7c3f41e43aa92
pharreX19 <pharreX19@gmail.com>
2025-02-20 23:14:45 +0500

    feat: added soft delete to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ namespace App\Models;
 use App\Models\Role;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
