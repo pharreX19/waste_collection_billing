d24bdb19a4ae8ef405b2244b488dd2aa34c61ca4
pharreX19 <pharreX19@gmail.com>
2025-02-22 01:51:53 +0500

    feat: bpg webhook controller added

---
+++ app/Http/Controllers/BPGWebhookController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\BPG\WebhookAction;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class BPGWebhookController
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $payload = $request->all();
+
+        $validation_rules = [
+            "id" => 'required|string',
+            "local_id" => 'required|string',
+            "customer_reference" => 'required|string',
+            // "state" => "required|string|in:CONFIRMED",
+        ];
+
+        $validator = Validator::make($payload['data'], $validation_rules);
+
+        if ($validator->failed()) {
+            throw new ValidationException($validator);
+        }
+
+        if ($payload['data']['state'] === 'PP_TRANSACTION_GENERATED') {
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return (new WebhookAction())->execute($payload['data']);
+    }
+}
