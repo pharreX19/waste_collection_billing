a9d3316a4173f9246e7e6e53c619fbc1945d9db4
pharreX19 <pharreX19@gmail.com>
2025-02-23 12:01:33 +0500

    fix: pagination labels

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -15,14 +15,22 @@
                         ? 'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-medium text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                         : 'relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0',
                 ]"
-                v-html="link.label"
-            ></Link>
+            >
+                <div>
+                    {{
+                        index == 0
+                            ? "ފަހަތަށްް"
+                            : index == props.links.length - 1
+                            ? "ކުރިއަށް"
+                            : link.label
+                    }}
+                </div></Link
+            >
         </nav>
     </div>
 </template>
 
 <script setup>
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
 import { Link } from "@inertiajs/vue3";
 
 const props = defineProps({
